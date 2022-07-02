76e36106323488935f61f952f8f5467832a8f4fa
Seifeldin Zook <Seifeldin.zook@gmail.com>
2022-07-03 01:51:20 +0200

    191.Numberof1Bits and 7.ReverseInteger

--- Medium/7.ReverseInteger.c
+++ Medium/7.ReverseInteger.c
@@ -1,22 +1,23 @@
-// 7. Reverse Integer
-
 /*
-Given a signed 32-bit integer x, return x with its digits reversed. 
-If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+  7. Reverse Integer
 
-Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+  Given a signed 32-bit integer x, return x with its digits reversed. 
+  If reversing x causes the value to go outside 
+  the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
-Example 1:
-Input: x = 123
-Output: 321
+  Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
-Example 2:
-Input: x = -123
-Output: -321
+  Example 1:
+  Input: x = 123
+  Output: 321
 
-Example 3:
-Input: x = 120
-Output: 21
+  Example 2:
+  Input: x = -123
+  Output: -321
+
+  Example 3:
+  Input: x = 120
+  Output: 21
 */
 
 #include <limits.h>
@@ -51,5 +52,5 @@ int reverse(int x) {
 // }
 
 int main() {
-  printf("reverse: %d\n", reverse(0));
+  printf("reverse: %d\n", reverse(1231));
 }
