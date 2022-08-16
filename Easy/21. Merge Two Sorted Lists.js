7aee779c0cea1d4997613561ca10020554dc88a9
Seifeldin Zook <Seifeldin.zook@gmail.com>
2022-08-16 22:25:52 +0200

    21. Merge Two Sorted Lists

---
+++ Easy/21. Merge Two Sorted Lists.js
@@ -0,0 +1,70 @@
+/* 
+21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+var mergeTwoLists = function (l1, l2) {
+  let currentNode = new ListNode();
+  let head = list;
+
+  while (l1 !== null && l2 !== null) {
+    // Select the smallest value from either linked list,
+    // then increment that list forward.
+    if (l1.val < l2.val) {
+      currentNode.next = new ListNode(l1.val);
+      l1 = l1.next;
+    } else {
+      currentNode.next = new ListNode(l2.val);
+      l2 = l2.next;
+    }
+    currentNode = currentNode.next;
+  }
+  // It's possible that one linked list is shorter than the other so we just
+  // add on the remainder of the last linked list. It's already sorted :)
+  if (l1 !== null) list.next = l1;
+  if (l2 !== null) list.next = l2;
+
+  // return .next because this first element in the linkedlist is empty
+  return head.next;
+};
