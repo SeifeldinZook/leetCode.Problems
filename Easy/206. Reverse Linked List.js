4bf123ea2cbad06f79474cbcc1456d2f26140cbd
Seifeldin Zook <Seifeldin.zook@gmail.com>
2022-08-24 03:17:55 +0200

    206. Reverse Linked List

---
+++ Easy/206. Reverse Linked List.js
@@ -0,0 +1,62 @@
+/* 
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+ 
+
+Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (!head || !head.next) return head;
+
+  var reversed = new ListNode(head.val);
+
+  let i = 0;
+  while (head) {
+    currentNode = new ListNode(head.val);
+
+    if (i !== 0) {
+      let tmp = reversed;
+      reversed = currentNode;
+      reversed.next = tmp;
+    }
+    head = head.next;
+    i++;
+  }
+  return reversed;
+};
